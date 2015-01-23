02f6d80d0d0bf619b7ada64361c7110cc11f35a5
asa47@hotmail.it <asa47@hotmail.it@9cfc30a6-d8ec-f28b-62bf-b182faeb68da>
2015-01-23 23:43:45 +0000

    implemented Ojectives controller, Laser

---
+++ Russell/Assets/_script/Ojectives/DisplayOjective.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class DisplayOjective : MonoBehaviour {
+
+	public int indexOjective = 0;
+
+	private GameObject objectivesController;
+	private float timer = 0;
+
+	void Start()
+	{
+		objectivesController = GameObject.FindGameObjectWithTag ("ObjectiveController");
+	}
+
+	void OnTriggerStay (Collider other) {
+		if(other.gameObject.tag == "Player" && timer < 5.0f)
+		{
+			objectivesController.GetComponent<Image> ().color = Color.Lerp (objectivesController.GetComponent<Image> ().color, new Color(1f, 1f, 1f, 1f), 2.0f * Time.deltaTime);
+			objectivesController.GetComponent<ObjectivesController>().index = indexOjective;
+			objectivesController.GetComponent<ObjectivesController>().isActive = true;
+			timer += Time.deltaTime;
+		}else
+		{
+			objectivesController.GetComponent<ObjectivesController>().isActive = false;
+		}
+	}
+
+	void OnTriggerExit (Collider other) {
+		if(other.gameObject.tag == "Player"){
+			objectivesController.GetComponent<ObjectivesController>().isActive = false;
+			timer = 0;
+		}
+	}
+
+
+
+
+}
