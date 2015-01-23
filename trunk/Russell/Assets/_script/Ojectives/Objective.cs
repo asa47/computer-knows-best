7d9336625b73a932c91e9179c141c21bff8feec9
asa47@hotmail.it <asa47@hotmail.it@9cfc30a6-d8ec-f28b-62bf-b182faeb68da>
2015-01-23 23:43:45 +0000

    implemented Ojectives controller, Laser

---
+++ trunk/Russell/Assets/_script/Ojectives/Objective.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Objective {
+
+	public string description;
+	public bool completed;
+	public int points;
+
+	public Objective (string _description, int _points, bool completed = false)
+	{
+		description = _description;
+		points = _points;
+
+	}
+
+}
---
+++ trunk/Russell/Assets/_script/Player/PlayerRaycast.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerRaycast : MonoBehaviour {
+
+	private float timer = 0;
+	private bool laserOn = false;
+	private ParticleSystem particle;
+
+	// Use this for initialization
+	void Start () {
+		particle = GameObject.FindGameObjectWithTag ("Laser").GetComponent<ParticleSystem> ();
+		particle.Stop ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(!laserOn && Input.GetMouseButtonDown(0)){
+			particle.Play();
+			laserOn = true;
+		}
+
+		if(laserOn)
+		{
+			timer += Time.deltaTime;
+			RaycastHit hit;
+			if(Physics.Raycast (transform.position, transform.forward, out hit, 10.0f)) {
+				//Debug.Log(hit.transform.name);
+			}
+
+			if(timer > 0.5){
+				timer = 0;
+				laserOn = false;
+				particle.Stop();
+			}
+
+		} 
+	}
+
+
+}
